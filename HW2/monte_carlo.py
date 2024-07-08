74443c8d8057d537ab76e46cb8c34636b740fc61
DapperDataAnalyst <rwmson@utexas.edu>
2024-07-07 20:08:50 -0500

    n_step not working

--- HW2/monte_carlo.py
+++ HW2/monte_carlo.py
@@ -5,37 +5,11 @@ from env import EnvSpec
 from policy import Policy
 
 def off_policy_mc_prediction_ordinary_importance_sampling(
-    env_spec:EnvSpec,
-    trajs:Iterable[Iterable[Tuple[int,int,int,int]]],
-    bpi:Policy,
-    pi:Policy,
-    initQ:np.array
-) -> np.array:
-    """
-    input:
-        env_spec: environment spec
-        trajs: N trajectories generated using
-            list in which each element is a tuple representing (s_t,a_t,r_{t+1},s_{t+1})
-        bpi: behavior policy used to generate trajectories
-        pi: evaluation target policy
-        initQ: initial Q values; np array shape of [nS,nA]
-    ret:
-        Q: $q_pi$ function; numpy array shape of [nS,nA]
-    """
-
-    #####################
-    # TODO: Implement Off Policy Monte-Carlo prediction algorithm using ordinary importance
-    # sampling (Hint: Sutton Book p. 109, every-visit implementation is fine)
-    #####################
-
-    return Q
-
-def off_policy_mc_prediction_weighted_importance_sampling(
-    env_spec:EnvSpec,
-    trajs:Iterable[Iterable[Tuple[int,int,int,int]]],
-    bpi:Policy,
-    pi:Policy,
-    initQ:np.array
+    env_spec: EnvSpec,
+    trajs: Iterable[Iterable[Tuple[int, int, int, int]]],
+    bpi: Policy,
+    pi: Policy,
+    initQ: np.array
 ) -> np.array:
     """
     input:
@@ -49,9 +23,69 @@ def off_policy_mc_prediction_weighted_importance_sampling(
         Q: $q_pi$ function; numpy array shape of [nS,nA]
     """
 
-    #####################
-    # TODO: Implement Off Policy Monte-Carlo prediction algorithm using weighted importance
-    # sampling (Hint: Sutton Book p. 110, every-visit implementation is fine)
-    #####################
+    Q = initQ.copy()
+    C = np.zeros_like(Q)  # Initialize C(s, a) = 0
+
+    gamma = env_spec.gamma
+    
+    for episode in trajs:
+        G = 0
+        W = 1
+        
+        for t in range(len(episode) - 1, -1, -1):
+            s_t, a_t, r_t1, s_t1 = episode[t]
+            G = gamma * G + r_t1
+            C[s_t, a_t] += W
+            Q[s_t, a_t] += (W / C[s_t, a_t]) * (G - Q[s_t, a_t])
+            
+            # Update W
+            if pi.action_prob(s_t, a_t) == 0:
+                break
+            W *= pi.action_prob(s_t, a_t) / bpi.action_prob(s_t, a_t)
+            if W == 0:
+                break
+
+    return Q
+
+def off_policy_mc_prediction_weighted_importance_sampling(
+    env_spec: EnvSpec,
+    trajs: Iterable[Iterable[Tuple[int, int, int, int]]],
+    bpi: Policy,
+    pi: Policy,
+    initQ: np.array
+) -> np.array:
+    """
+    input:
+        env_spec: environment spec
+        trajs: N trajectories generated using behavior policy bpi
+            list in which each element is a tuple representing (s_t,a_t,r_{t+1},s_{t+1})
+        bpi: behavior policy used to generate trajectories
+        pi: evaluation target policy
+        initQ: initial Q values; np array shape of [nS,nA]
+    ret:
+        Q: $q_pi$ function; numpy array shape of [nS,nA]
+    """
+
+    Q = initQ.copy()
+    C = np.zeros_like(Q)  # Initialize C(s, a) = 0
+
+    gamma = env_spec.gamma
+    
+    for episode in trajs:
+        G = 0
+        W = 1
+        
+        for t in range(len(episode) - 1, -1, -1):
+            s_t, a_t, r_t1, s_t1 = episode[t]
+            G = gamma * G + r_t1
+            C[s_t, a_t] += W
+            Q[s_t, a_t] += (W / C[s_t, a_t]) * (G - Q[s_t, a_t])
+            
+            # Update W
+            if pi.action_prob(s_t, a_t) == 0:
+                break
+            W *= pi.action_prob(s_t, a_t) / bpi.action_prob(s_t, a_t)
+            if W == 0:
+                break
 
     return Q
--- HW2/n_step_bootstrap.py
+++ HW2/n_step_bootstrap.py
@@ -5,57 +5,102 @@ from env import EnvSpec
 from policy import Policy
 
 def on_policy_n_step_td(
-    env_spec:EnvSpec,
-    trajs:Iterable[Iterable[Tuple[int,int,int,int]]],
-    n:int,
-    alpha:float,
-    initV:np.array
+    env_spec: EnvSpec,
+    trajs: Iterable[Iterable[Tuple[int, int, int, int]]],
+    n: int,
+    alpha: float,
+    initV: np.array
 ) -> Tuple[np.array]:
     """
     input:
         env_spec: environment spec
         trajs: N trajectories generated using
-            list in which each element is a tuple representing (s_t,a_t,r_{t+1},s_{t+1})
+            list in which each element is a tuple representing (s_t, a_t, r_{t+1}, s_{t+1})
         n: how many steps?
         alpha: learning rate
         initV: initial V values; np array shape of [nS]
     ret:
-        V: $v_pi$ function; numpy array shape of [nS]
+        V: $v_\pi$ function; numpy array shape of [nS]
     """
 
-    #####################
-    # TODO: Implement On Policy n-Step TD algorithm
-    # sampling (Hint: Sutton Book p. 144)
-    #####################
+    V = initV.copy()
+    gamma = env_spec.gamma
+
+    for episode in trajs:
+        T = len(episode)
+        states = [step[0] for step in episode] + [episode[-1][3]]  # S_0 to S_T
+        rewards = [step[2] for step in episode]  # R_1 to R_T
+
+        for t in range(T):
+            tau = t - n + 1
+            if tau >= 0:
+                G = sum(gamma**(i - tau) * rewards[i] for i in range(tau, min(tau + n, T)))
+                if tau + n < T:
+                    G += gamma**n * V[states[tau + n + 1]]
+                V[states[tau]] += alpha * (G - V[states[tau]])
 
     return V
 
+
 def off_policy_n_step_sarsa(
-    env_spec:EnvSpec,
-    trajs:Iterable[Iterable[Tuple[int,int,int,int]]],
-    bpi:Policy,
-    n:int,
-    alpha:float,
-    initQ:np.array
-) -> Tuple[np.array,Policy]:
+    env_spec: EnvSpec,
+    trajs: Iterable[Iterable[Tuple[int, int, int, int]]],
+    bpi: Policy,
+    n: int,
+    alpha: float,
+    initQ: np.array
+) -> Tuple[np.array, Policy]:
     """
     input:
         env_spec: environment spec
         trajs: N trajectories generated using
-            list in which each element is a tuple representing (s_t,a_t,r_{t+1},s_{t+1})
+            list in which each element is a tuple representing (s_t, a_t, r_{t+1}, s_{t+1})
         bpi: behavior policy used to generate trajectories
         pi: evaluation target policy
         n: how many steps?
         alpha: learning rate
-        initQ: initial Q values; np array shape of [nS,nA]
+        initQ: initial Q values; np array shape of [nS, nA]
     ret:
-        Q: $q_star$ function; numpy array shape of [nS,nA]
+        Q: $q_star$ function; numpy array shape of [nS, nA]
         policy: $pi_star$; instance of policy class
     """
 
-    #####################
-    # TODO: Implement Off Policy n-Step SARSA algorithm
-    # sampling (Hint: Sutton Book p. 149)
-    #####################
+    Q = initQ.copy()
+    gamma = env_spec.gamma
+    nS, nA = env_spec.nS, env_spec.nA
+
+    def greedy_policy(Q):
+        class GreedyPolicy(Policy):
+            def action_prob(self, state: int, action: int) -> float:
+                return 1.0 if action == np.argmax(Q[state]) else 0.0
+
+            def action(self, state: int) -> int:
+                return np.argmax(Q[state])
+        
+        return GreedyPolicy()
+
+    pi = greedy_policy(Q)  # Initial policy is greedy with respect to Q
+
+    for episode in trajs:
+        T = len(episode)
+        states = [step[0] for step in episode] + [episode[-1][3]]  # S_0 to S_T
+        actions = [step[1] for step in episode] + [0]  # A_0 to A_T, add dummy action at the end
+        rewards = [step[2] for step in episode]  # R_1 to R_T
+
+        for t in range(T):
+            s_t, a_t, r_t1, s_t1 = episode[t]
+            actions[t] = bpi.action(s_t)
+            actions[t + 1] = bpi.action(s_t1)
+
+            tau = t - n + 1
+            if tau >= 0:
+                rho = np.prod([pi.action_prob(states[i], actions[i]) / bpi.action_prob(states[i], actions[i]) for i in range(tau, min(tau + n, T))])
+                G = sum(gamma**(i - tau) * rewards[i] for i in range(tau, min(tau + n, T)))
+                if tau + n < T:
+                    G += gamma**n * Q[states[tau + n], actions[tau + n]]
+                Q[states[tau], actions[tau]] += alpha * rho * (G - Q[states[tau], actions[tau]])
+
+                # Update policy to be greedy with respect to Q
+                pi = greedy_policy(Q)
 
     return Q, pi
